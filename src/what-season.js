4fbdc9bfca829f1482ce4171bb0b956d53c62e7e
shamkolovich <shamkolovich95@mail.ru>
2022-10-18 02:05:10 +0300

    feat: all

--- src/what-season.js
+++ src/what-season.js
@@ -11,25 +11,25 @@
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(date) {
-  console.log(date)
+ function getSeason(date) {
   // remove line with error and write your code here
   let result = ['spring', 'summer', 'autumn', 'winter'];
-  let month = date.getMonth();
-  if (Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date.getTime())) {
-    if (month === 11 || month < 2) {
-      return result[3];
-    } else if (month < 5) {
-      return result[0];
-    } else if (month < 8) {
-      return result[1];
-    } else if (month < 11) {
-      return result[2];
-    }
-  }
-  throw new Error('Invalid date!');
+  if(date) {
+    if (date.__proto__ === Date.prototype) {
+      let month = date.getMonth();
+      if (month === 11 || month < 2) {
+        return result[3];
+      } else if (month < 5) {
+        return result[0];
+      } else if (month < 8) {
+        return result[1];
+      } else if (month < 11) {
+        return result[2];
+      }
+    } else throw new Error('Invalid date!');
+  } else return 'Unable to determine the time of year!';
 }
-
+//console.log(getSeason('foo'))
 
 module.exports = {
   getSeason
